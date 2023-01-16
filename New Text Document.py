ff9252d4dcc424fad0e8264aa36114213fc544c7
rajeshpatil <rajesh.patil@insofe.edu.in>
2023-01-16 14:47:15 +0530

    1st commit

---
+++ New Text Document.py
@@ -0,0 +1,12 @@
+
+
+list1 = [2,4,6,8,10]
+
+square = [X**2 for i in list1]
+
+print('Square of the numbers are:',square] 
+
+cube = [X**3 for i in list1]
+
+print('Cube of the numbers are :',cube]
+
